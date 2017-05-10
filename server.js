bb1d09ab363e839c8cee46dce06abb83679b325b
José David Saborío Marín <jdavidsaboriom@outlook.com>
2017-05-09 19:40:14 -0600

    creamos eventemitter request and listening

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 'use strict'
 
 const http = require('http');
+
 // var de entorno
 const port = process.env.PORT || 8080
-//crear un servidor web HTTP
 
+//crear un servidor web HTTP
 const server = http.createServer();
 
-server.listen(port);
+//eventos eventemitter
+server.on('request',onRequest);
+server.on('listening',onListening);
+
+server.listen(port)
+
+function onRequest(req,res){
+  res.end("Hola io.js");
+}
+
+function onListening(){
+  console.log("Servidor escuchando en el port "+ port);
+}
