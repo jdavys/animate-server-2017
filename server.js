d83ad9b3a12f2031d5b19f223413fd661395e94b
José David Saborío Marín <jdavidsaboriom@outlook.com>
2017-05-09 20:08:35 -0600

    Se crea index.html en la carpeta public y respondemos con file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 
 // var de entorno
 const port = process.env.PORT || 8080
+const fs = require('fs')
 
 //crear un servidor web HTTP
 const server = http.createServer();
@@ -15,7 +16,9 @@ server.on('listening',onListening);
 server.listen(port)
 
 function onRequest(req,res){
-  res.end("Hola io.js");
+  //res.end("Hola io.js");
+  let file = fs.readFileSync('public/index.html');
+  res.end(file)
 }
 
 function onListening(){
