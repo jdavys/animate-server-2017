010167c5f7f87d0f0381421020987d1dafbb74ba
José David Saborío Marín <jdavidsaboriom@outlook.com>
2017-05-09 20:22:27 -0600

    Se utiliza modulo path para las rutas

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const http = require('http');
 // var de entorno
 const port = process.env.PORT || 8080
 const fs = require('fs')
+//manejo de rutas
+const path = require('path');
 
 //crear un servidor web HTTP
 const server = http.createServer();
@@ -17,8 +19,16 @@ server.listen(port)
 
 function onRequest(req,res){
   //res.end("Hola io.js");
-  let file = fs.readFileSync('public/index.html');
-  res.end(file)
+  //let file = fs.readFileSync('public/index.html');
+  //asincrono join concatenar rutas y dire
+  let fileName= path.join(__dirname,'public','index.html');
+  fs.readFile(fileName,function(err,file){
+    if(err){
+      return res.end(err.message);
+    }
+    res.end(file)
+  });
+
 }
 
 function onListening(){
