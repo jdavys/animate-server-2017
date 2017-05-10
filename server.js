a0d7503298b8ed7bac18c50cd38a43eb081d9e74
José David Saborío Marín <jdavidsaboriom@outlook.com>
2017-05-09 21:43:21 -0600

    servir un archivo js en el cliente creadon

--- server.js
+++ server.js
@@ -18,14 +18,24 @@ server.on('listening',onListening);
 server.listen(port)
 
 function onRequest(req,res){
+  let uri=req.url;
+  if(uri.startsWith('/index') || uri ==='/'){
+    return serverIndex(res);
+  }
+
+  if( uri==='/app.js'){
+    return serverApp(res);
+  }
+  res.statusCode=404
+  res.end(`404 not found ${uri}`)
   //res.end("Hola io.js");
   //let file = fs.readFileSync('public/index.html');
   //asincrono join concatenar rutas y dire
-  let index= path.join(__dirname,'public','index.html');
+  //let index= path.join(__dirname,'public','index.html');
 
-  res.setHeader('content-Type', 'text/html');
+  //res.setHeader('content-Type', 'text/html');
   //crear streams mecamismos leer archivos binario
-  let rs=fs.createReadStream(index) //lectura
+  //let rs=fs.createReadStream(index) //lectura
 
   //eventemitter streams no es recomendado
   /*rs.on('data',function(chunk){
@@ -33,11 +43,11 @@ function onRequest(req,res){
   })*/
 
   //pipe request stream de lect, respo stream escritura
-  rs.pipe(res)
+  /*rs.pipe(res)
 
   rs.on('error',function(err){
     res.end(err.message)
-  })
+  })*/
   /*fs.readFile(index,function(err,file){
     if(err) return res.end(err.message);
 
@@ -48,6 +58,29 @@ function onRequest(req,res){
 
 }
 
+function serverIndex(res){
+  let index= path.join(__dirname,'public','index.html')
+  res.setHeader('content-Type', 'text/html')
+  let rs=fs.createReadStream(index)
+  rs.pipe(res)
+
+  rs.on('error',function(err){
+    res.setHeader('content-Type', 'text/plain')
+    res.end(err.message)
+  })
+}
+
+function serverApp(res){
+  let app= path.join(__dirname,'public','app.js')
+  res.setHeader('content-Type', 'text/javascript')
+  let rs=fs.createReadStream(app)
+  rs.pipe(res)
+
+  rs.on('error',function(err){
+    res.setHeader('content-Type', 'text/plain')
+    res.end(err.message)
+  })
+}
 function onListening(){
   console.log(`Servidor escuchando en el port ${port}` )
 }
